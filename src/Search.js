18f72e9b9508c51dfb12edf9c25afd1906cd5e36
Htibbs14 <htibbs14@gmail.com>
2022-03-29 22:45:44 -0600

    Made search engine functional

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  let [searchWord, setSearchWord] = useState();
+
+  function wordSearch(event) {
+    event.preventDefault();
+    alert(`Searching for ${searchWord}`);
+  }
+
+  function wordInput(event) {
+    setSearchWord(event.target.value);
+  }
+
   return (
     <div className="Search">
       <div className="container form-container">
-        <form>
+        <form onSubmit={wordSearch}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Type a word"
+                placeholder="Type a word..."
                 className="form-control search-bar"
+                onChange={wordInput}
               />
             </div>
             <div className="col-3">
-              <input type="submit" className="btn btn-primary" />
+              <input type="submit" className="btn btn-primary" value="Search" />
             </div>
           </div>
         </form>
