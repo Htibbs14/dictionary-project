4369fd2af65bad3bc1c7b53496347ce7a03e27f7
Htibbs14 <htibbs14@gmail.com>
2022-03-31 15:15:10 -0600

    Added footer and some spacing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-        <h1>Dictionary</h1>
-        <h2>Which word would you like to search for?</h2>
-        <Search />
-      </div>
+      <h1>Dictionary</h1>
+      <h2>Which word would you like to search for?</h2>
+      <Search />
+      <p className="footer">
+        {" "}
+        <a href="https://github.com/Htibbs14/dictionary-project">
+          Open-source code
+        </a>{" "}
+        by Hailey Tibbens
+      </p>
+    </div>
   );
 }
 
