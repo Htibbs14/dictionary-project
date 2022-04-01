20dfee4ceb6d62555efd70ec163528be54923e12
Htibbs14 <htibbs14@gmail.com>
2022-03-31 18:31:24 -0600

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   if (props.phonetics.audio) {
     return (
       <div className="Phonetics">
-        <a href={props.phonetics.audio} target="_blank">
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         <br />
